adb136f5d556ea298af8e424a460323059c5a8b9
joshuatuan <itsjoshuatuan+git@gmail.com>
2025-02-09 23:03:11 +0800

    Refined user prop to include only necessary fields

--- app/features/dashboard/page.tsx
+++ app/features/dashboard/page.tsx
@@ -1,40 +1,23 @@
-import { deleteAccountAction, signOutAction } from "@/lib/actions/auth";
-import { Button } from "@/components/ui/button";
 import { getUser } from "@/lib/data-service";
-import AlertButton from "@/components/alertButton";
 import { redirect } from "next/navigation";
+import Dashboard from "@/components/features/Dashboard";
 
 export default async function DashBoardPage() {
-  const user = await getUser();
+  const userData = await getUser();
 
-  if (!user) {
+  if (!userData) {
     return redirect("/");
   }
 
-  return (
-    <div className="mx-auto flex flex-col gap-10">
-      <div>
-        <h1 className="mb-6 text-2xl font-semibold">
-          Logged in as <span className="font-bold">{user.email}</span>
-        </h1>
-        <p className="mb-2 text-sm text-muted-foreground">Account details</p>
-        <pre className="max-h-64 max-w-96 justify-center overflow-auto rounded border p-3 font-mono text-xs md:max-w-xl">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
+  const user = {
+    email: userData.email,
+    id: userData.id,
+    created_at: userData.created_at,
+  };
 
-      <div className="flex gap-4">
-        <AlertButton>
-          <form action={deleteAccountAction}>
-            <Button type="submit">Delete Account</Button>
-          </form>
-        </AlertButton>
-        <form action={signOutAction}>
-          <Button type="submit" variant={"outline"}>
-            Sign out
-          </Button>
-        </form>
-      </div>
-    </div>
+  return (
+    <>
+      <Dashboard user={user} />
+    </>
   );
 }
